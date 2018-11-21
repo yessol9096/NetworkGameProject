44bfa00098e44ac4e1d41a5e42785b904295fe03
Chaeyoung2 <xxll0331@hanmail.net>
2018-11-21 15:39:38 +0900

    recvn 함수 추가

--- MapleStory/Headers/Function.h
+++ MapleStory/Headers/Function.h
@@ -36,3 +36,24 @@ void err_display(const char *msg)
 	MessageBox(NULL, (LPCTSTR)lpMsgBuf, (LPCTSTR)msg, MB_ICONERROR);
 	LocalFree(lpMsgBuf);
 }
+
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while (left > 0) {
+		received = recv(s, ptr, left, flags);
+
+		if (received == SOCKET_ERROR)
+			return SOCKET_ERROR;
+
+		else if (received == 0)
+			break;
+
+		left -= received;
+		ptr += received;
+	}
+	return len - left;
+}
